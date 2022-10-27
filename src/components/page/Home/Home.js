b21ac59f1cf44d9a73d8c96bdc5d3de7b77b83e6
meshkatKanok <meshkatkanok12@gmail.com>
2022-10-27 19:36:29 +0600

    home

---
+++ src/components/page/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Corusel from '../../BrabnCorusel/Corusel';
+import Googlegit from '../../Googlegitlogin/Googlegit';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Corusel></Corusel>
+
+            <div>
+               
+                <Googlegit></Googlegit>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Home;
