6d4c680a33c108dd27013f0d09fc38c1a2511c89
meshkatKanok <meshkatkanok12@gmail.com>
2022-10-27 20:40:07 +0600

    react-to-pdf

--- src/components/page/Catagorey/Catagorey.js
+++ src/components/page/Catagorey/Catagorey.js
@@ -3,28 +3,17 @@ import { Link, useLoaderData} from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import { Button } from 'react-bootstrap';
-import Axios from "axios";
-import FileDownload from '../Catagorey/Catagorey'
+import ReactToPdf from "react-to-pdf"
+ 
 const Catagorey = () => {
    const cataData=useLoaderData()
    const {title,image_url,details,_id
 
    }=cataData
-   const download=(e)=>{
-    e.preventDefault()
-    Axios({
-      url:"http://localhost:3000/",
-      method:"GET",
-      responseType:"blob"
-    })
-    .then(res=>{
-      FileDownload(res.data,"downloded.png")
-    })
-   }
-     
+   const ref = React.createRef(); 
     return (
         <div>
-            <CardGroup>
+            <CardGroup ref={ref}>
       <Card>
         <Card.Img variant="top" src={image_url} />
         <Card.Body>
@@ -34,7 +23,13 @@ const Catagorey = () => {
           </Card.Text>
         </Card.Body>
         <Card.Footer className='d-flex justify-content-between'>
-        <Button onClick={(e)=>download(e)} className='w-25 mt-2 mt-lg-0 ' variant="primary">Download pdf</Button>
+        <ReactToPdf x={20} y={0.5}>
+
+    {({toPdf, targetRef}) =>  (
+        <Button className='w-25 mt-2 mt-lg-0' variant="primary" onClick={toPdf} ref={targetRef}>Download pdf</Button>
+    )}
+</ReactToPdf>
+        
         <Button className='w-50 mt-2 mt-lg-0' variant="primary">
         <Link className='text-white' to={`/checkou/${_id}`} style={{textDecoration:'none'}}>CheckOut</Link>
           </Button>
